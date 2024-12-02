4e4cb9099c13eae7d83f3ebee7e771788b81339a
Moosa Azfar <moosa.azfar@gmail.com>
2024-12-01 22:20:02 -0800

    remove debug from tracking

--- Debug/myDrivers/q7seg/q7seg.d
+++
@@ -1,9 +0,0 @@
-myDrivers/q7seg/q7seg.o: ../myDrivers/q7seg/q7seg.c \
- ../myDrivers/q7seg/q7seg.h \
- C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/gpio/gpio.h \
- C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/common.h \
- C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/rcc/rcc.h
-../myDrivers/q7seg/q7seg.h:
-C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/gpio/gpio.h:
-C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/common.h:
-C:/Users/moosa/STM32CubeIDE/workspace_1.17.0/sensors/myDrivers/hal/rcc/rcc.h:
