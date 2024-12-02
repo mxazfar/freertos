245f2b48646c4c427f99e8b088c1ffc90fc6af44
Moosa Azfar <moosa.azfar@driveseres.com>
2024-12-02 11:32:18 -0800

    create basic timer lib

---
+++ myDrivers/hal/timers/tim_general.c
@@ -0,0 +1,31 @@
+#include "tim_general.h"
+#include "stm32f446xx.h"
+
+tim_ret_t configureTimer(tim_general_timer_t timer, tim_general_config_t *timerConfiguration) {
+    TIM_TypeDef* timerBase = timGeneral2to5PtrArray[timer];
+
+    timerBase->CR1 |= (timerConfiguration->clockDivision << TIM_CR1_CKD_Pos) | 
+                      (timerConfiguration->countStyle << TIM_CR1_DIR_Pos) |
+                      (1 << TIM_CR1_URS_Pos); // hardcoding this for now, only under/overflow generate interrupt
+
+    timerBase->PSC = timerConfiguration->prescaleAmount;
+    timerBase->ARR = timerConfiguration->preloadValue;
+
+    return timSuccess;
+}
+
+tim_ret_t startTimer(tim_general_timer_t timer) {
+    TIM_TypeDef* timerBase = timGeneral2to5PtrArray[timer];
+
+    timerBase->CR1 |= (1 << TIM_CR1_CEN_Pos);
+
+    return timSuccess;
+}
+
+tim_ret_t stopTimer(tim_general_timer_t timer) {
+    TIM_TypeDef* timerBase = timGeneral2to5PtrArray[timer];
+
+    timerBase->CR1 &= ~(1 << TIM_CR1_CEN_Pos);
+
+    return timSuccess;
+}
---
+++ myDrivers/hal/timers/tim_general.h
@@ -0,0 +1,42 @@
+#ifndef TIM_GENERAL_H
+#define TIM_GENERAL_H
+
+#include <stdint.h>
+#include "stm32f446xx.h"
+
+TIM_TypeDef* timGeneral2to5PtrArray[4] = {TIM2, TIM3, TIM4, TIM5};
+
+typedef uint16_t tim_general_prescaler_t;
+typedef uint32_t tim_general_load_t;
+
+typedef enum {
+    timGeneralTimer2,   // 32 bit timer
+    timGeneralTimer3,   // 16 bit timer
+    timGeneralTimer4,   // 16 bit timer
+    timGeneralTimer5    // 32 bit timer
+} tim_general_timer_t;
+
+typedef enum {
+    timGeneralCountUp,
+    timGeneralCountDown
+} tim_general_count_paradigm_t;
+
+typedef enum {
+    timGenCkInt,
+    timGenCkIntTimes2,
+    timGenCkTntTimes4
+} tim_general_clk_div_t;
+
+typedef struct {
+    tim_general_count_paradigm_t countStyle;
+    tim_general_prescaler_t prescaleAmount;
+    tim_general_clk_div_t clockDivision;
+    tim_general_load_t preloadValue
+} tim_general_config_t;
+
+typedef enum {
+    timFail,
+    timSuccess
+} tim_ret_t;
+
+#endif /* TIM_GENERAL_H */
