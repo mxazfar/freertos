cf1676d37478f7bdb31bf7f6e7a96fe8871e41ef
Moosa Azfar <moosa.azfar@gmail.com>
2024-12-02 21:59:26 -0800

    flesh out adc driver

---
+++ myDrivers/hal/adc/adc.c
@@ -0,0 +1,49 @@
+#include "stm32f446xx.h"
+#include "adc.h"
+#include "rcc.h"
+
+#define ADC_GET_UNIT_ADDR(adcUnit) ((uintptr_t)ADC1 + (ADC_UNIT_REGISTER_OFFSET*adcUnit))
+
+adc_ret_t configureAdc(adc_unit_t unit, adc_config_t *adcConfig) {
+    ADC_TypeDef *adcUnit = ADC_GET_UNIT_ADDR(unit);
+
+    // supply clock
+    rcc_enable_peripheral_clk(rccClkAdc1 + unit);
+
+    // set scan mode
+    adcUnit->CR2 |= (adcConfig->scanMode << ADC_CR2_CONT_Pos);
+    // set interrupt enable EOCIE in ADC_CR1
+    adcUnit->CR1 |= ADC_CR1_EOCIE;
+
+    // write pin(s) to be converted and set sample times
+    for(uint16_t i = 0; i < adcConfig->numChannels; i++) {
+        // set sample times
+        if(adcConfig->channels[i] < 10) {
+            adcUnit->SMPR2 |= adcConfig->sampleTime[i] << adcConfig->channels[i] * 3;
+        } else {
+            adcUnit->SMPR1 |= adcConfig->sampleTime[i] << (adcConfig->channels[i] * 3 - 10);
+        }
+
+        // write pin sequence
+        if(i < 6) {
+            adcUnit->SQR3 |= adcConfig->channels[i] << i * 5;
+        } else if (i < 13) {
+            adcUnit->SQR2 |= adcConfig->channels[i] << (i * 5 - 30);
+        } else {
+            adcUnit->SQR1 |= adcConfig->channels[i] << (i * 5 - 60);
+        }
+    }
+
+    // turn it on
+    adcUnit->CR2 |= ADC_CR2_ADON;
+
+    return adcSuccess;
+}
+
+adc_ret_t triggerSample(adc_unit_t unit) {
+    ADC_TypeDef *adcUnit = ADC_GET_UNIT_ADDR(unit);
+
+    adcUnit->CR2 |= ADC_CR2_SWSTART;
+
+    return adcSuccess;
+}
--- myDrivers/hal/adc/adc.h
+++ myDrivers/hal/adc/adc.h
@@ -1,6 +1,10 @@
 #ifndef ADC_H
 #define ADC_H
 
+#include <stdint.h>
+
+#define ADC_UNIT_REGISTER_OFFSET 0x100U
+
 typedef enum {
     adcTim1Ch1,
     adcTim1Ch2,
@@ -26,5 +30,60 @@ typedef enum {
     adcTriggerFalling,
     adcTriggerRisingFalling
 } adc_trigger_polarity_t;
+
+typedef enum {
+    adc1,
+    adc2,
+    adc3
+} adc_unit_t;
+
+typedef enum {
+    adcFail,
+    adcSuccess
+} adc_ret_t;
+
+typedef enum {
+    adcSingleConverion,
+    adcContinuous
+} adc_scan_mode_t;
+
+typedef enum {
+    adcCH1,
+    adcCH2,
+    adcCH3,
+    adcCH4,
+    adcCH5,
+    adcCH6,
+    adcCH7,
+    adcCH8,
+    adcCH9,
+    adcCH10,
+    adcCH11,
+    adcCH12,
+    adcCH13,
+    adcCH14,
+    adcCH15,
+    adcCH16,
+    adcCH17,
+    adcCH18
+} adc_channel_t;
+ 
+typedef enum {
+    adc3Cycles,
+    adc15Cycles,
+    adc28Cycles,
+    adc56Cycles,
+    adc84Cycles,
+    adc112Cycles,
+    adc144Cycles,
+    adc480Cycles
+} adc_sample_time_t;
+
+typedef struct {
+    adc_scan_mode_t scanMode; // mode of scanning (single or continuous)
+    adc_channel_t *channels; // pointer to array of channels to sample (must be in sequence order)
+    adc_sample_time_t *sampleTime; // pointer to sample time of each channel (must be in sequence order)
+    uint8_t numChannels; // number of channels
+} adc_config_t;
 
 #endif /* ADC_H */
